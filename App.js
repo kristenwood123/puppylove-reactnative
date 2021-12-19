6cdc906ccaae357be4eaa1a33b63b6c9195f36f0
Kristen <kristenwood123@gmail.com>
2021-12-18 16:21:47 -0800

    added card styles

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, Image, View, StyleSheet } from 'react-native'
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes'
 
 const App = () => {
   return (
-    <Text>Hello World</Text>
+    <View style={styles.pageContainer}>
+        <View style={styles.card}>
+          <Image 
+            source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg'
+          }}
+            style={styles.image}
+          />
+      </View>
+    </View>
   )
 }
 
+const styles = StyleSheet.create({
+  pageContainer: { 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    flex: 1
+  },
+  card: {
+    width: '95%',
+    height: '70%',
+    borderRadius: 10
+  },
+  image: {
+    width: '100%', 
+    height: '100%',
+    borderRadius: 10
+  }
+})
 export default App
