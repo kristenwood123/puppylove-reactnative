4efaaf4232b81faf18a4c16f109ae68d4491510c
Kristen <kristenwood123@gmail.com>
2021-12-19 16:05:30 -0800

    edited card

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, Image, View, StyleSheet } from 'react-native'
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes'
+import { Icon } from 'react-native-elements'
+import { data }  from './assets/data/dogs'
 
 const App = () => {
+  const [dogs, setDogs] = useState(data)
+
   return (
     <View style={styles.pageContainer}>
-        <View style={styles.card}>
-          <Image 
-            source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg'
-          }}
-            style={styles.image}
-          />
+        <View style={styles.card}> 
+            <Image 
+              source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg'}}
+              style={styles.image}
+            />
+          <Text style={styles.name}>Spark</Text>
+          <Text style={styles.breed}>Husky</Text>
+          <Icon
+            name='heart'
+            type='font-awesome'
+            color='red'
+            />
+          <Icon
+            name='times'
+            type='font-awesome'
+            color='black'
+            />
       </View>
     </View>
   )
@@ -24,13 +38,34 @@ const styles = StyleSheet.create({
   },
   card: {
     width: '95%',
-    height: '70%',
-    borderRadius: 10
+    height: '85%',
+    borderRadius: 10,
+    shadowColor: '#000',
+    backgroundColor: 'pink',
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11
   },
   image: {
-    width: '100%', 
-    height: '100%',
+    width: '90%', 
+    height: '70%',
+    padding: 10,
     borderRadius: 10
+  },
+  name: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }, 
+  breed: {
+    fontSize: 16,
+    color: 'gray',
+    textAlign: 'center',
+    lineHeight: 25
   }
 })
 export default App
---
+++ assets/data/dogs.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    id: 'eirou5jds',
+    name: 'Max',
+    breed: 'Husky',
+    image: require('../images/dog3.jpg'),
+    percent: 50
+  },
+   {
+    id: 'eirou3jds',
+    name: 'Bear',
+    breed: 'Husky',
+    image: require('../images/dog4.jpg'),
+    percent: 90
+  },
+   {
+    id: 'eirou7jds',
+    name: 'Bella',
+    breed: 'Husky',
+    image: './images/dog3.jpg',
+    percent: 81
+  },
+   {
+    id: 'eirou9jds',
+    name: 'Luna',
+    breed: 'Husky',
+    image: './images/dog4.jpg',
+    percent: 43
+  },
+   {
+    id: 'ei2roujds',
+    name: 'Bailey',
+    breed: 'Husky',
+    image: './images/dog5.jpg',
+    percent: 76
+  },
+   {
+    id: 'eiro4ujds',
+    name: 'Rocky',
+    breed: 'Husky',
+    image: './images/dog6.jpg',
+    percent: 89
+  },
+   {
+    id: 'eirou7jds',
+    name: 'Tucker',
+    breed: 'Husky',
+    image: './images/dog7.jpg',
+    percent: 10
+  },
+   {
+    id: 'eirouj9ds',
+    name: 'Jack',
+    breed: 'Husky',
+    image: './images/dog8.jpg',
+    percent: 45
+  },
+   {
+    id: '3eiroujds',
+    name: 'Stella',
+    breed: 'Husky',
+    image: './images/dog9.jpg',
+    percent: 23
+  },
+   {
+    id: 'eiroujds0',
+    name: 'Milo',
+    breed: 'Husky',
+    image: './images/dog10.jpg',
+    percent: 50
+  }
+]
+
+export default data
