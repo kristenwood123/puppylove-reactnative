8c310be1b808e152663d2d65619dfb837109b4ef
Kristen <kristenwood123@gmail.com>
2021-12-20 04:02:47 -0800

    gesture handler

--- App.js
+++ App.js
@@ -4,35 +4,64 @@ import 'react-native-gesture-handler'
 import Animated, { 
   useAnimatedStyle, 
   useSharedValue,
-  withSpring
+  withSpring,
+  useAnimatedGestureHandler,
+  useDerivedValue,
+  interpolate
  } from 'react-native-reanimated'
+ import { PanGestureHandler } from 'react-native-gesture-handler'
 
 // Data 
 import dogs  from './assets/data/dogs'
 // Components
 import Card from './src/components/Card'
+import { useWindowDimensions } from 'react-native'
 
 const App = () => {
-  const sharedValue = useSharedValue(1)
+  const { width: screenWidth } = useWindowDimensions()
+  const translateX = useSharedValue(0)
+  const rotate = useDerivedValue(() => interpolate(
+    translateX.value, 
+    [0, screenWidth],
+    [0, 60]
+  ) + 'deg'
+  )
 
   const cardStyle = useAnimatedStyle(() => ({
     transform: [
       {
-      translateX: sharedValue.value * 200,
-    },
+        translateX: translateX.value,
+      },
+      {
+        rotate: rotate.value,
+      }
   ],
   }));
 
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, context) => {
+      context.startX = translateX.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = context.startX + event.translationX;
+    },
+    onEnd: () => {
+      console.warn('Touch ended');
+    }
+  })
+
   return (
     <View style={styles.pageContainer}>
-      <Animated.View style={[styles.animatedCard, cardStyle]}>
-        <Card dog={dogs[0]} />
-      </Animated.View>
-      <Pressable 
-        onPress={() => (sharedValue.value = withSpring(Math.random()))}
-        style={{position: 'absolute', top: 250}}>
-        <Text>Change Value</Text>
-      </Pressable>
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+          <Animated.View style={[styles.animatedCard, cardStyle]}>
+            <Card dog={dogs[0]} />
+          </Animated.View>
+        </PanGestureHandler>
+        <Pressable 
+          onPress={() => (sharedValue.value = withSpring(Math.random()))}
+          style={{position: 'absolute', top: 250}}>
+          <Text>Change Value</Text>
+        </Pressable>
     </View>
   )
 }
